a676e863acbc5885a6c2bff185bdec02c229b875
nathan <nathdagh@hotmail.com>
2026-04-10 21:39:24 +0200

    structs and move semanctics

--- exercises/06_move_semantics/move_semantics2.rs
+++ exercises/06_move_semantics/move_semantics2.rs
@@ -1,9 +1,7 @@
-fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
-    let mut vec = vec;
-
-    vec.push(88);
-
-    vec
+fn fill_vec(vec: &mut Vec<i32>) -> Vec<i32> {
+    let mut res = vec.clone();
+    res.push(88);
+    res
 }
 
 fn main() {
@@ -18,9 +16,9 @@ mod tests {
     // fix the compiler error in the test.
     #[test]
     fn move_semantics2() {
-        let vec0 = vec![22, 44, 66];
+        let mut vec0 = vec![22, 44, 66];
 
-        let vec1 = fill_vec(vec0);
+        let vec1 = fill_vec(&mut vec0);
 
         assert_eq!(vec0, [22, 44, 66]);
         assert_eq!(vec1, [22, 44, 66, 88]);
